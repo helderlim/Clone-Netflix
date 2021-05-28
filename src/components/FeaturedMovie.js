8461333c5f7393c941214033ab54e57d4407d426
helderlim <helderfelima16@gmail.com>
2021-05-28 18:59:21 -0300

    feat: condicional para plural

--- src/components/FeaturedMovie.js
+++ src/components/FeaturedMovie.js
@@ -15,7 +15,7 @@ export default ({item}) =>{
                     <div className="featured--info">
                         <div className="featured--points">{item.vote_average} pontos</div>
                         <div className="feature--year">2099</div>
-                        <div className="feature-seasons">{item.number_of_seasons} temporadas</div>
+                        <div className="feature-seasons">{item.number_of_seasons} temporada{item.number_of_seasons !== 1 ? 's' : ''}</div>
                     </div>
                 </div>
             </div>
