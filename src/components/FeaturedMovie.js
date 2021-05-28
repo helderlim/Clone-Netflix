47e962f2045aa0f318b046365b36d0a591af66ec
helderlim <helderfelima16@gmail.com>
2021-05-28 18:57:55 -0300

    feat: apresentando infos destaque

--- src/components/FeaturedMovie.js
+++ src/components/FeaturedMovie.js
@@ -9,7 +9,16 @@ export default ({item}) =>{
             backgroundPosition: 'center',
             backgroundImage: `url(https://image.tmdb.org/t/p/original${item.backdrop_path})`
         }}>
-            <div>{item.original_name}</div>
+            <div className="featured--vertical">
+                <div className="featured--horizontal">
+                    <div className="featured--name">{item.original_name}</div>
+                    <div className="featured--info">
+                        <div className="featured--points">{item.vote_average} pontos</div>
+                        <div className="feature--year">2099</div>
+                        <div className="feature-seasons">{item.number_of_seasons} temporadas</div>
+                    </div>
+                </div>
+            </div>
         </section>
     )
 }
