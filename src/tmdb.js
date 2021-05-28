8a91e62e717b485c4492a8a5c015fea9af9794d0
helderlim <helderfelima16@gmail.com>
2021-05-28 14:58:19 -0300

    feat: chamando a api para o console

--- src/tmdb.js
+++ src/tmdb.js
@@ -1,7 +1,7 @@
 const API_KEY = 'c5979ab4574fac6dc1d602debc6c1e5a';
 const API_BASE = 'https://api.themoviedb.org/3';
 
-const basicFetch = async =(endpoint) => {
+const basicFetch = async (endpoint) => {
     const req = await fetch(`${API_BASE}${endpoint}`);
     const json = await req.json();
     return json; 
