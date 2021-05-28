41efe106df1e198ed4e51080b7b000d84d3f7e13
helderlim <helderfelima16@gmail.com>
2021-05-28 19:03:38 -0300

    feat: mais infos

--- src/components/FeaturedMovie.js
+++ src/components/FeaturedMovie.js
@@ -14,9 +14,14 @@ export default ({item}) =>{
                     <div className="featured--name">{item.original_name}</div>
                     <div className="featured--info">
                         <div className="featured--points">{item.vote_average} pontos</div>
-                        <div className="feature--year">2099</div>
-                        <div className="feature-seasons">{item.number_of_seasons} temporada{item.number_of_seasons !== 1 ? 's' : ''}</div>
+                        <div className="featured--year">2099</div>
+                        <div className="featured-seasons">{item.number_of_seasons} temporada{item.number_of_seasons !== 1 ? 's' : ''}</div>
                     </div>
+                    <div className="featured--description">{item.overview}</div>
+                    <div className="featured--buttons">
+
+                    </div>
+                    <div className="featured--genres"><strong>Gêneros: </strong>...</div>
                 </div>
             </div>
         </section>
