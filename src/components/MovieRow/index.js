6d4c0648aa368ff883d8673664469a22f54e9c05
helderlim <helderfelima16@gmail.com>
2021-05-29 16:28:53 -0300

    feat: botoes de navegação

--- src/components/MovieRow/index.js
+++ src/components/MovieRow/index.js
@@ -1,10 +1,17 @@
 import './MovieRow.css';
-
+import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
+import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos';
 import React from 'react';
 
 const  MovieRow = ({title, items}) =>{
     return(
         <div className="movieRow">
+            <div className="movieRow--left">
+                <ArrowBackIosIcon style={{fontSize: 50}}/>
+            </div>
+            <div className="movieRow--right">
+                <ArrowForwardIosIcon style={{fontSize: 50}}/>
+            </div>
             <h2>{title}</h2>
             <div className="movieRow--listarea">
                 <div className="movieRow--list">
